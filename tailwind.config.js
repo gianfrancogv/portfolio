9e0024da14505902d33939de5b9ed5e7bc6cfa5a
gianfrancogv <gianfrancogv@gmail.com>
2022-01-11 13:18:32 +0100

    Installing Tailwind and creating navbar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+
+    colors: {
+      'primaryblue': '#364F6B',
+      'secondaryred': '#FC5185'
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif']
+    }
+
+  },
+  plugins: [],
+}

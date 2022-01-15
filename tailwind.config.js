98384e27901d99160bff2e8db7462f0203153cae
gianfrancogv <gianfrancogv@gmail.com>
2022-01-15 09:56:22 +0100

    Skills & Experience layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-
     colors: {
       'primaryblue': '#364F6B',
-      'secondaryred': '#FC5185'
+      'secondaryred': '#FC5185',
+      'fontgray': '#333'
     },
     fontFamily: {
       poppins: ['Poppins', 'sans-serif']
     }
-
   },
   plugins: [],
 }

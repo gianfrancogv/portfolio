97dbf3711b87e76c04392b06dae30d8332e7c703
gianfrancogv <gianfrancogv@gmail.com>
2022-02-08 14:37:20 +0100

    Dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   content: ['./index.html'],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
         'primaryblue': '#364F6B',
         'secondaryred': '#FC5185',
-        'fontgray': '#333'
+        'fontgray': '#333',
+        'darkbg': '#243447'
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
